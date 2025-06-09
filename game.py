877d1f9950fda76146062dc4ffdd59b378778cd3
LanpherKa <kapenniman@philasd.org>
2025-06-09 12:16:47 -0400

    version_3

--- game.py
+++ game.py
@@ -26,6 +26,21 @@ def menu():
                 if event.pos[0] in range(275, 400) and event.pos[1] in range(225, 350):
                     game()
 
+def win():
+    win = pygame.image.load('assets\win.png')
+    win = pygame.transform.scale(win, (640, 480))
+    while True:
+        screen.blit(win,(0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] in range(275, 400) and event.pos[1] in range(225, 350):
+                    game()
+
+
 #game function called by menu
 def game():
     numbers = ['assets/zero.png', 'assets/one.png', 'assets/two.png', 'assets/three.png', 'assets/four.png', 'assets/five.png']
@@ -99,6 +114,8 @@ def game():
                     pygame.mixer.Sound.play(wrong_sound)
                     pygame.mixer.music.stop()
                     
+        if score >= 10:
+            win()
 
         pygame.display.update()
         for event in pygame.event.get():
