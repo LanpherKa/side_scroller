65b6c9e7091db184456a1102246f9d4a27e8ffb9
LanpherKa <kapenniman@philasd.org>
2025-06-09 11:34:05 -0400

    version 2

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Side Scroller Game")
 font = pygame.font.Font(None, 50)
 text_surface = font.render("0", True, "white")
-text_rect = text_surface.get_rect(center=(275, 150))
+text_rect = text_surface.get_rect(center=(250, 150))
+text_surface2 = font.render("0", True, "white")
+text_rect2 = text_surface2.get_rect(center=(500, 450))
 
 
 #the menu starts the game 
@@ -33,17 +35,27 @@ def game():
     player = pygame.image.load('assets/characterR.png')
     player = pygame.transform.rotozoom(player, 0, 0.7)
     player_y = 300
-    gravity = .75
+    gravity = .5
     jumpcount = 0
     jump = 0
-    addend1 = random.randint(0, 10)
-   
+    addend1 = random.randint(5, 10)
+    addend2 = random.randint(0, 5)
+    target = pygame.image.load(numbers[addend2])
+    target = pygame.transform.rotozoom(target, 0, 0.5)
+    target_x = 500
+    score = 0
+    correct_sound = pygame.mixer.Sound("assets/correct_sound1.wav")
+    wrong_sound = pygame.mixer.Sound("assets/wrong.wav")
+
     while True:
         screen.blit(image,(bgx-640,0))
         screen.blit(image, (bgx, 0))
         screen.blit(image, (bgx+640, 0))
-        text_surface = font.render("Target: " + str(addend1), True, "white")
+        text_surface = font.render( str(addend1) + " + ? = 10", True, "white")
         screen.blit(text_surface, text_rect)
+
+        text_surface2 = font.render( "Score: " + str(score), True, "white")
+        screen.blit(text_surface2, text_rect2)
 
         bgx = bgx - 0.25
         if bgx <= -640:
@@ -59,6 +71,34 @@ def game():
                 jumpcount = 0
                 jump = 0
 
+        screen.blit(target, (target_x, 375))
+        if target_x < 0:
+            target_x = 600
+            addend2 = random.randint(0, 5)
+            target = pygame.image.load(numbers[addend2])
+            target = pygame.transform.rotozoom(target, 0, 0.5)
+        else:
+            target_x -= 0.5
+
+        if target_x > 30 and target_x < 80:
+            if player_y > 250:
+                if (addend1 + addend2 ) == 10:
+                    #print("correct " + str(addend1) + " + " + str(addend2))
+                    target_x = 600
+                    addend2 = random.randint(0, 5)
+                    target = pygame.image.load(numbers[addend2])
+                    target = pygame.transform.rotozoom(target, 0, 0.5)
+                    addend1 = random.randint(5, 10)
+                    score = score + 1
+                    pygame.mixer.Sound.play(correct_sound)
+                    pygame.mixer.music.stop()
+                    #text_surface = font.render( str(addend1) + " + ? = 10", True, "white")
+                else:
+                    score = score -1 
+                    target_x = 600
+                    pygame.mixer.Sound.play(wrong_sound)
+                    pygame.mixer.music.stop()
+                    
 
         pygame.display.update()
         for event in pygame.event.get():
